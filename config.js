e373b28607b5c564d2276a8761acb53295aafeea
vgmono1 <87943965+vgmono1@users.noreply.github.com>
2021-07-26 22:06:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,16 @@ const CONFIG = {
     Almost people will leave and think it quite werid.
     But can you countinue it until end?`,
     btnIntro: 'I had something to talk with you. ',
-    title: 'I go crazy because of you, baby. My heart beats fast when I am thinking of you.',
-    desc: 'I dont know why I miss you all day and night. Do you wanna have a boyfriend? ',
-    btnYes: 'Yes <33',
-    btnNo: 'Không, Anh trai àxcxzc :3',
-    question:'Txxzczxcxzrên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu xzz không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay zczxzxzxzxzx7h xcxcxzanh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii luxcxzn <3',
-    messLink: 'http://fb.comzxxzzxc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 
+    'I go crazy because of you, baby. My heart beats fast when I am thinking of you.',
+    desc: 'I dont know why I miss you all day and night. Do you wanna have a date with me? ',
+    btnYes: 'Yes, I do <33',
+    btnNo: 'No, bro :3',
+    question:' Type to see more <3',
+    btnReply: ' Press to leave.',
+    reply: ' I love you so much <33333333',
+    mess: ' Hehe I know 🥰. Love you too 😘😘',
+    messDesc: 'Good night my soulmate. ',
+    btnAccept: ' You too. (●’◡’●)',
+    messLink: 'https://www.instagram.com/vgmono1/'
 }
