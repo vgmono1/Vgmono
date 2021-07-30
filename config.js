8c74d43d8533c1f760291a089bd777b98a0e234e
vgmono1 <87943965+vgmono1@users.noreply.github.com>
2021-07-30 21:59:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'Hi, the cutiest girl in my heart.            (*^▽^*)',
     introDesc: `I know the way I send you this link is pretty impolite.
-    Almost people will leave and think it quite werid.
-    But can you countinue it until end?`,
+     Almost people will leave and think it quite werid.
+     But can you continue it until end?`,
     btnIntro: 'I had something to talk with you. ',
     title: 
     'I go crazy because of you, baby. My heart beats fast when I am thinking of you.',
